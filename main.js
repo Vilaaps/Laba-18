88cb6c16e45669dc2f2e1ec205c298e64d08314b
vika <vikatekutova7@gmail.com>
2026-03-20 16:20:24 +0300

    Step 6: variables and data types in JavaScript

---
+++ main.js
@@ -0,0 +1,67 @@
+// console.log("Hello JavaScript!");
+// let age = 20;
+// let name = "Denis";
+// let isStudent = true;
+// console.log("Name: ", name);
+// console.log("Age: ", age);
+// console.log("IS student: ", isStudent);
+// let value = 10;
+// console.log(value);
+// value = "Теперь эта строка";
+// console.log(value);
+// value = true;
+// console.log(value);
+// let userName = "Alekseu";
+// console.log(`Hello, ${userName}!`);
+// let price = 99.99;
+// let temperature = -15;
+// let infinity = 1 / 0;
+// let notANumber = 0 / 0;
+// console.log(0.1 + 0.2);
+// let digNumber = 9007365592346758n;
+// let huge = BigInt("12318495913405906676")
+// let isAlive = true;
+// let isWorking = false;
+// let isAdult = age >= 18;
+// let x;
+// let y = undefined;
+// let userData = null;
+// let id = Symbol("id");
+// let person = {
+//     name: "Stanislav",
+//     age: 30,
+//     isStudent: false,
+//     sayHello: function () {
+//         console.log("Hello!");
+//     },
+// };
+// console.log(person.name);
+// let fruits = ["яблоко","банан" ,"апельсин"];
+// let numbers = [1,2,3,4,5];
+// let mixed = ["текст",42 ,true ,null];
+// function sum(a,b) {
+//     return a + b;
+// }
+// let multiply = function (x,y) {
+//     return x*y
+// };
+// console.log(sum(5,3))
+// let now = new Date();
+// let birthday = new Date("1995-12-17");
+// let a = 10;
+// let b = 3;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(10 + "5");
+// console.log("10" - 5);
+const numbersArray = [1,2,3];
+numbersArray[0] = 10;
+console.log(numbersArray);
+numbersArray = [5,6,7];
+const persons = {name: "Denis", age: 18};
+persons.age = 50;
+persons.city = "Volgogred";
+console.log(persons);
+person = {name: "Stas"};
